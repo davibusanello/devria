9322d7e4bf0e1e730094aa55bbc95a4eff76936b
Davi Busanello <contato@davibusanello.net>
2012-10-27 02:03:55 -0200

    adicionados README e tela de login com  extjs feita em aula com dojo

--- aula3/app.js
+++ aula3/app.js
@@ -22,63 +22,57 @@ Ext.onReady(function() {
 			allowBlank:false
 		}],
 		buttons:[{
-
 			text:'Entrar',
 			formBind: true,
 			handler: facaLogin
 		}]
-		);
-
-		var win = Ext.create("Ext.Window",{
-			layout:'fit',
-			title: 'Acesso ao Sistema Caixa 1.0',
-			width: 280,
-			heigth: 140,
-			resizable: false, 
-			closable: false,
-			draggable: false,
-			plain: true,
-			border: false,
-			items:[login]
-		});
-
-		win.Show();
-
-		function facaLogin() {
-			if (login.getForm().isValid()){
-				login.getForm().submit({
-					method: 'POST',
-					waitTitle: 'Por favor aguarde',
-					waitMsg: 'Autenticando no sistema',
-					sucess: function(){
-						login.getForm().reset();
-						var redirect = 'principal.php';
-						window.location = redirect;
-					};
-					failure: function(){
-						if (action.failureType === 'server'){
-							obj = Ext.decode(action.response.resposeText);
-							Ext.Msg.show({
-								title: 'Erro no login',
-								msg: obj.erro.motivo,
-								butoons: Ext.Msg.OK,
-								icon: Ext.MessageBox.ERROR,
-								scope:this,
-								width: 150,
-							})
-						}
-						else
-						{
-							Ext.Mag.alert("Erro no cliente")
-						}
-						login.getform().reset();
-					}
-				});
-
-			}
-		}
-
-
-
 	});
+
+	var win = Ext.create("Ext.Window",{
+		layout:'fit',
+		title: 'Acesso ao Sistema Caixa 1.0',
+		width: 280,
+		heigth: 140,
+		resizable: false, 
+		closable: false,
+		draggable: false,
+		plain: true,
+		border: false,
+		items:[login]
+	});
+
+	win.show();
+
+	function facaLogin() {
+		if (login.getForm().isValid()){
+			login.getForm().submit({
+				method: 'POST',
+				waitTitle: 'Por favor aguarde',
+				waitMsg: 'Autenticando no sistema',
+				sucess: function(){
+					login.getForm().reset();
+					var redirect = 'principal.php';
+					window.location = redirect;
+				},
+				failure: function(){
+					if (action.failureType === 'server'){
+						obj = Ext.decode(action.response.resposeText);
+						Ext.Msg.show({
+							title: 'Erro no login',
+							msg: obj.erro.motivo,
+							butoons: Ext.Msg.OK,
+							icon: Ext.MessageBox.ERROR,
+							scope:this,
+							width: 150,
+						})
+					}
+					else
+					{
+						Ext.Mag.alert("Erro no cliente")
+					}
+					login.getform().reset();
+				}
+			});
+		}
+	}
 });
